e9aaa06de4f41a61c50b9c571a54cd381a2b8bba
AlexdrCastillo <alexdrcastillo9gmail.com>
2024-05-20 17:09:28 -0400

    cambios 2

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,7 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(120), nullable=False)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,7 +5,7 @@ from app.models import User, db
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.json
-    new_user = User(username=data['username'], email=data['email'])
+    new_user = User(username=data['username'], email=data['email'], password=data['password'])  # Corregir typo
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'User created successfully'})
@@ -13,5 +13,5 @@ def create_user():
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    result = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+    result = [{'id': user.id, 'username': user.username, 'email': user.email, 'password': user.password } for user in users]
     return jsonify(result)
